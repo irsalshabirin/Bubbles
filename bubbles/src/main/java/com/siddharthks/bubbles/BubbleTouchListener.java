40f7e8efd5a75df9f3784a68a5e69959565052d1
siddharth2010 <ssiddharth2010@gmail.com>
2019-03-17 14:43:36 +0530

    set up basic blueprint via interface

---
+++ bubbles/src/main/java/com/siddharthks/bubbles/BubbleTouchListener.java
@@ -0,0 +1,15 @@
+package com.siddharthks.bubbles;
+
+public interface BubbleTouchListener {
+
+    void onDown(float x, float y);
+
+    void onTap(boolean expanded);
+
+    void onRemove();
+
+    void onMove(float x, float y);
+
+    void onUp(float x, float y);
+
+}
