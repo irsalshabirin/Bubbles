cdd478c382e566ba513aaee9cf71fe4a22d99ad3
siddharth2010 <ssiddharth2010@gmail.com>
2019-03-17 15:14:43 +0530

    Version 1.0 Working

---
+++ bubbles/src/main/java/com/siddharthks/bubbles/BubbleLogger.java
@@ -0,0 +1,36 @@
+package com.siddharthks.bubbles;
+
+import android.util.Log;
+
+public class BubbleLogger {
+
+    private boolean isDebugEnabled;
+    private String tag;
+
+    public BubbleLogger() {
+        isDebugEnabled = false;
+        tag = BubbleLogger.class.getSimpleName();
+    }
+
+    public BubbleLogger setTag(String tag) {
+        this.tag = tag;
+        return this;
+    }
+
+    public BubbleLogger setDebugEnabled(boolean enabled) {
+        this.isDebugEnabled = enabled;
+        return this;
+    }
+
+    public void log(String message) {
+        if (isDebugEnabled) {
+            Log.d(tag, message);
+        }
+    }
+
+    public void log(String message, Throwable throwable) {
+        if (isDebugEnabled) {
+            Log.e(tag, message, throwable);
+        }
+    }
+}
