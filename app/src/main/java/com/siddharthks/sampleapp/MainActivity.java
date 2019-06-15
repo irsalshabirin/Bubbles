8586f97a2a3fd172a9b6f574bfe6397b1fd45099
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-16 03:32:26 +0530

    Add visual polish to main activity

--- app/src/main/java/com/siddharthks/sampleapp/MainActivity.java
+++ app/src/main/java/com/siddharthks/sampleapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.siddharthks.sampleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.siddharthks.bubbles.FloatingBubblePermissions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingBubblePermissions.startPermissionRequest(this);
+        View startBubble = findViewById(R.id.start_bubble);
+        startBubble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(getApplicationContext(), SimpleService.class));
+            }
+        });
     }
 }
