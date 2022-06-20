efe70b2171e6da68b623283958eeca27c87c4847
Irsal Shabirin <irsal.shabirin@gojek.com>
2022-06-20 13:25:54 +0700

    add kotlin dependency and migrate to androidx

--- app/src/main/java/com/siddharthks/sampleapp/MainActivity.java
+++ app/src/main/java/com/siddharthks/sampleapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.siddharthks.sampleapp;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
